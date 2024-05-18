98255c9d99ecf706c7bcd064f02d6448cc91d9e9
Carlos Yañez Martinez <61806656+yancarmtz@users.noreply.github.com>
2024-05-18 11:08:59 -0600

    Update crearProducto.js

--- js/crearProducto.js
+++ js/crearProducto.js
@@ -1,30 +1,51 @@
 import { conectaAPI } from "./conectaAPI.js";
 
 const formulario = document.querySelector("[data-formulario]");
+const productContainer = document.querySelector("[data-lista]");
 
-//validaciones
-
-async function crearCard(evento){
-    evento.preventDefault();
-    
-    const nombre = document.querySelector("[data-nombre]").value;
-    const precio = document.querySelector("[data-precio]").value;
-    const imagen = document.querySelector("[data-imagen]").value;   
-
-    console.log("crea producto")
-    
-    try{
-        await conectaAPI.crearCard(nombre,precio,imagen);
-        
-        console.log("Carga OK");
-      
-    }catch(e){
-        alert(e);
-        console.log("Carga NOK");
-    }   
+function createCard(id, name, price, image) {
+    const card = document.createElement("li");
+    card.innerHTML = `
+    <figure class="card">
+    <img class="producto-imagen"
+        aspect-ratio: auto
+        height="60px"
+        src="${image}"
+        alt="${name}">
+    <figcaption class="card-container--info">
+        <p class="card-container--title">${name}</p>
+        <div class="card-container--value">
+            <p>$ ${price}</p>
+            <img class="card-container--icon" src="./assets/icons/borrar.png" data-remove="true" data-id="${id}"/>
+        </div>
+    </figcaption>
+</figure>
+        </figure>
+    `;
+    return card;
 }
 
-formulario,addEventListener("submit", evento => crearCard(evento)); 
-
+const render = async () => {
+    try {
+        const listProducts = await conectaAPI.crearCard();
+        if (listProducts && Array.isArray(listProducts)) {
+            listProducts.forEach(product => {
+                productContainer.appendChild(
+                    createCard(
+                        product.id,
+                        product.name,
+                        product.price,
+                        product.image
+                        
+                    )
+                );
+            });
+        } else {
+            console.error('Expected an array of products, but got:', listProducts);
+        }
+    } catch (error) {
+        console.error('Error in render function:', error);
+    }
+};
 
 

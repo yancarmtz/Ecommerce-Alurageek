8debbeeba8e306819b01b39f5f61e611455735c5
Carlos Yañez Martinez <61806656+yancarmtz@users.noreply.github.com>
2024-05-18 11:27:13 -0600

    Update crearProducto.js

--- js/crearProducto.js
+++ js/crearProducto.js
@@ -3,19 +3,19 @@ import { conectaAPI } from "./conectaAPI.js";
 const formulario = document.querySelector("[data-formulario]");
 const productContainer = document.querySelector("[data-lista]");
 
-function createCard(id, name, price, image) {
+function createCard(id, nombre, precio, imagen) {
     const card = document.createElement("li");
     card.innerHTML = `
     <figure class="card">
     <img class="producto-imagen"
         aspect-ratio: auto
         height="60px"
-        src="${image}"
-        alt="${name}">
+        src="${imagen}"
+        alt="${nombre}">
     <figcaption class="card-container--info">
         <p class="card-container--title">${name}</p>
         <div class="card-container--value">
-            <p>$ ${price}</p>
+            <p>$ ${precio}</p>
             <img class="card-container--icon" src="./assets/icons/borrar.png" data-remove="true" data-id="${id}"/>
         </div>
     </figcaption>
@@ -33,9 +33,9 @@ const render = async () => {
                 productContainer.appendChild(
                     createCard(
                         product.id,
-                        product.name,
-                        product.price,
-                        product.image
+                        product.nombre,
+                        product.precio,
+                        product.imagen
                         
                     )
                 );
@@ -48,4 +48,19 @@ const render = async () => {
     }
 };
 
+
+// Captura los datos del formulario
+formulario.addEventListener("submit", async (event) => {
+    event.preventDefault();
+    const nombre = document.querySelector("[data-nombre]").value;
+    const precio = document.querySelector("[data-precio]").value;
+    const imagen = document.querySelector("[data-imagen]").value;
+
+    conectaAPI.crearCard(nombre, precio, imagen)
+    .then((res) => {
+        console.log(res);
+        window.location.reload(); 
+    })
+    .catch((err) => console.log(err));
+});
 
